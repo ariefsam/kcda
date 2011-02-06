bb9ab748c2d8ba3366414f271433504dcd49ef1f
ariefhidayatulloh <ariefhidayatulloh@3abe02a9-46e5-f76a-ef0b-8d5d6dffac7b>
2011-02-06 10:52:03 +0000

    Penyempurnaan dan grafik

---
+++ kcda/system/application/views/login.php
@@ -0,0 +1,14 @@
+<div>
+    <form action="perbaharui/login_p" method="post">
+    <table>
+        <tr>
+            <td>Username</td>
+            <td><input type="text" name="username" /></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td><input type="password" name="password" /></td>
+        </tr>
+    </table>
+    </form>
+</div>
